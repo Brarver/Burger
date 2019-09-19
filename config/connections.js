bc3236b796e4fb76b322d61438ab1e1275f9b7d1
brarver <brarver@yahoo.com>
2019-09-19 15:24:31 -0600

    file work

--- config/connections.js
+++ config/connections.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "addison73",
-    database: "day_planner_db"
+    database: "burgers_db"
   });
   
   connection.connect(function(err) {
