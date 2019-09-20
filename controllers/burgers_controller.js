93b6245adecb2cd0d1e9aa5046af79e52b7fe021
brarver <brarver@yahoo.com>
2019-09-20 09:03:57 -0600

    route get config

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger")
+var burger = require("../models/burger.js")
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create(["name", "devour"], [req.body.name, req.body.sleepy], function(result) {
+  burger.insertOne(["name", "devour"], [req.body.name, req.body.sleepy], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
       devour: req.body.sleepy
     },
